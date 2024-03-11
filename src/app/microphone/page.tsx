8596253ec3292af001a8aff5642d48aad3cff303
SeffconDev <allmindsdev@seffcon.com>
2024-03-11 14:48:05 +0530

    CSS updated

--- src/app/microphone/page.tsx
+++ src/app/microphone/page.tsx
@@ -52,7 +52,7 @@ const HookDemo = () => {
         </button>
         <div className="mb-4">
           <div className="flex items-center justify-between mb-2">
-            <span className="text-gray-900">Audio Level:</span>
+            <span className="text-gray-900">Audio Level: </span>
             <span className="text-gray-900">{audioLevel}</span>
           </div>
           <div className="w-full h-2 bg-gray-200 rounded-full">
